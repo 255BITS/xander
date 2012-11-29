c3ae9b33d855983ca61ad154c2898b451671bc0a
Mikkel Garcia <mikkel@255bits.com>
2012-11-28 23:28:57 -0700

    start script added with some details about how to test

---
+++ start.sh
@@ -0,0 +1,5 @@
+echo "Giles is servicing on port 2255 - please go to http://localhost:2255/qa.html"
+giles -s public/
+
+echo "If Giles is not installed, you can get it as an NPM module - instructions at http://www.github.com/255BITS/giles"
+
--- xander-api.coffee
+++
@@ -1,9 +0,0 @@
-class Xander
-  choose : (id) ->
-    result = []
-    for choice in @choices
-      result.push choice[0]
-    result
-
-module.exports = new Xander()
-
